68944426821347c0124ee5ae12a5af1708f72497
凉秋 <q1234553@outlook.com>
2026-06-03 20:43:06 +0800

    新增播放列表功能

--- music/Services/PlaybackService.cs
+++ music/Services/PlaybackService.cs
@@ -33,7 +33,7 @@ namespace music.Services
         public event EventHandler<double>? VolumeChanged;
 
         public bool IsPlaying => _isPlaying;
-        public Song? CurrentSong => _currentIndex >= 0 && _currentIndex < _playlist.Count ? _playlist[_currentIndex] : null;
+        public Song? CurrentSong { get; private set; }
         public List<Song> GetPlaylist() => new List<Song>(_playlist);
         public double Volume
         {
@@ -91,6 +91,7 @@ namespace music.Services
                         _mediaPlayer.Source = source;
                         _mediaPlayer.Play();
                         _isPlaying = true;
+                        CurrentSong = song;
                         CurrentSongChanged?.Invoke(this, song);
                         PlaybackStateChanged?.Invoke(this, true);
                     }
